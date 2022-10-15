29e12c4aec457ae5145069ebf663ee60f7d7a898
root <root@DESKTOP-5F505U6.localdomain>
2022-10-15 20:14:54 +0200

    init react

---
+++ src/component/layout/Layout.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Nav from "../nav/Nav";
+
+const Layout = () => {
+
+    return (
+        <>
+            <Nav />
+            <Routes>
+                
+            </Routes>
+        </>
+    );
+};
+
+export default Layout;
---
+++ src/component/nav/Nav.jsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+
+    return (
+        <>
+            <nav className="nav">
+                <img src="/img/logo.png" alt="logo meta dev 3" />
+            </nav>
+        </>
+    );
+};
+
+export default Nav;
